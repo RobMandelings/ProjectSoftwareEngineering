fe2a3832cd2deee33642a8723a4ec3b137f6ec4a
Jasper De Laet <jdlaet@gmail.com>
2020-03-26 16:59:06 +0100

    TEST input update

--- src/tests/inputTest.cpp
+++ src/tests/inputTest.cpp
@@ -9,5 +9,15 @@
 #include "simulation_utils.h"
 
 TEST(InputTest, FalseInput){
-    
+    MetroNet* metroNet = metro_parser::parseMetroNetXml("../src/tests/voorbeeldTestFalse.xml");
+    metro_utils::printMetroNet(metroNet, "../output/SummaryTestFalse.metro");
+
+    bool failed = false;
+
+    ofstream out("../output/SummaryTestFalse.metro");
+    if(out.fail()){
+        failed = true;
+    }
+
+    EXPECT_EQ(failed, true);
 }

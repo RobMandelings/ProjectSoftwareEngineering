cc55c6a02c86faa475810f1b4f563224e8e92cae
Jasper De Laet <jdlaet@gmail.com>
2020-03-26 17:14:00 +0100

    Test input fix

--- src/tests/inputTest.cpp
+++ src/tests/inputTest.cpp
@@ -9,15 +9,16 @@
 #include "simulation_utils.h"
 
 TEST(InputTest, FalseInput){
-    MetroNet* metroNet = metro_parser::parseMetroNetXml("../src/tests/voorbeeldTestFalse.xml");
-    metro_utils::printMetroNet(metroNet, "../output/SummaryTestFalse.metro");
 
     bool failed = false;
 
-    ofstream out("../output/SummaryTestFalse.metro");
-    if(out.fail()){
-        failed = true;
+    try{
+        MetroNet* metroNet = metro_parser::parseMetroNetXml("../src/tests/voorbeeldTestFalse.xml");
+        metro_utils::printMetroNet(metroNet, "../output/SummaryTestFalse.metro");
     }
+    catch(metro_parser::MetroNetParseException const& e){
+        failed = true;
+    };
 
     EXPECT_EQ(failed, true);
 }

85409bfe746e6dca589cb0cb46e5a31572b9148c
Jasper De Laet <jdlaet@gmail.com>
2020-03-26 16:41:08 +0100

    TEST inputTest

---
+++ src/tests/inputTest.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jasper on 26/03/2020.
+//
+
+#include <gtest.h>
+#include <fstream>
+#include "metro_utils.h"
+#include "metro_parser.h"
+#include "simulation_utils.h"
+
+TEST(InputTest, FalseInput){
+    
+}

ca498cd212e5d4654fddd90d7e97a763d5fd8b5d
RobMandelings <rob@minetronic.com>
2020-04-07 16:22:37 +0200

    Further implementation of track (spoor)

--- src/Track.cpp
+++ src/Track.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Track.h"
+#include "library.h"
+
+Track::Track(Station* sourceStation, Station* destinationStation) :
+        m_sourceStation(sourceStation),
+        m_destinationStation(destinationStation) {
+    ENSURE(sourceStation, "sourceStation may not be null!");
+    ENSURE(destinationStation, "destinationStation may not be null!");
+}
+
+Station* Track::getSourceStation() const {
+    REQUIRE(m_sourceStation, "sourceStation cannot be null!");
+    return m_sourceStation;
+}
+
+Station* Track::getDestinationStation() const {
+    REQUIRE(m_destinationStation, "destinationStation cannot be null!");
+    return m_destinationStation;
+}
+
+std::vector<Tram*> Track::getCurrentTrams() const {
+    return m_currentTrams;
+}
+
+void Track::addTram(Tram* tram) {
+    REQUIRE(tram, "the tram to be added to track should not be null!");
+    m_currentTrams.push_back(tram);
+}
--- src/Track.h
+++ src/Track.h
@@ -4,7 +4,57 @@
 
 #pragma once
 
+#include "vector"
+
+class Tram;
+
+class Station;
 
 class Track {
+
+private:
+
+    // TODO maybe add a length (which is calculated from the destinationStation and ?
+
+    /** The source station of this track (where the track starts) */
+    Station* m_sourceStation;
+
+    /** The destination station of this track (where the track ends) */
+    Station* m_destinationStation;
+
+    /** The trams which are currently riding on this track */
+    std::vector<Tram*> m_currentTrams;
+
+public:
+
+    /** @brief simple constructor
+     * @param sourceStation The source station of this track (where the track starts)
+     * @param destinationStation The destination station of this track (where the track ends)
+     */
+    Track(Station* sourceStation, Station* destinationStation);
+
+    /**
+     * @brief simple getter
+     * @return the source station of this track
+     */
+    Station* getSourceStation() const;
+
+    /**
+     * @brief simple getter
+     * @return the destination station of this track
+     */
+    Station* getDestinationStation() const;
+
+    /**
+     * @brief simple getter
+     * @return the trams currently riding on this track
+     */
+    std::vector<Tram*> getCurrentTrams() const;
+
+    /**
+     * @brief adds a tram to the trams currently riding on this tram (should be used by tram->updateLocation())
+     * @param tram the tram to add to the track
+     */
+    void addTram(Tram* tram);
 
 };

2c068034df19165422b4e040829592c071113dae
Thibault Poels <thibault.poels@gmail.com>
2020-04-09 21:30:06 +0200

    Platform member functions/variables

--- src/platform/Platform.cpp
+++ src/platform/Platform.cpp
@@ -3,3 +3,50 @@
 //
 
 #include "Platform.h"
+#include "stations/Station.h"
+#include "Track.h"
+
+Platform::Platform(Station *station, int number) :m_station(station), m_number(number){
+    Platform::_initCheck = this;
+    ENSURE(this->properlyInitialized(),"Constructor must end ...");
+}
+
+vector<Track*>& Platform::getTracks() {
+    REQUIRE(this->properlyInitialized(), "Platform must be properly initialized to use its member methods.");
+    return m_tracks;
+}
+
+void Platform::setTracks(const vector<Track*> &tracks) {
+    REQUIRE(this->properlyInitialized(), "Platform must be properly initialized to use its member methods.");
+    m_tracks = tracks;
+}
+
+Station* Platform::getStation() {
+    REQUIRE(this->properlyInitialized(), "Platform must be properly initialized to use its member methods.");
+    return m_station;
+}
+
+void Platform::setStation(Station* station) {
+    REQUIRE(this->properlyInitialized(), "Platform must be properly initialized to use its member methods.");
+    m_station = station;
+}
+
+int Platform::getNumber() const {
+    return m_number;
+}
+
+void Platform::setNumber(int number) {
+    REQUIRE(this->properlyInitialized(), "Platform must be properly initialized to use its member methods.");
+    m_number = number;
+}
+
+void Platform::addTrack(Track *track) {
+    REQUIRE(this->properlyInitialized(), "Platform must be properly initialized to use its member methods.");
+    m_tracks.push_back(track);
+}
+
+bool Platform::properlyInitialized() {
+    return Platform::_initCheck == this;
+}
+
+
--- src/platform/Platform.h
+++ src/platform/Platform.h
@@ -5,8 +5,42 @@
 #ifndef PROJECTSOFTWAREENGINEERING_PERRON_H
 #define PROJECTSOFTWAREENGINEERING_PERRON_H
 
+#include "library.h"
+
+class Station;
+class Track;
 
 class Platform {
+
+private:
+
+    Station* m_station;
+
+    int m_number;
+
+    vector<Track*> m_tracks;
+
+    Platform* _initCheck;
+
+public:
+
+    Platform(Station* station, int number);
+
+    vector<Track*>& getTracks();
+
+    void addTrack(Track* track);
+
+    void setTracks(const vector<Track*>& tracks);
+
+    Station *getStation();
+
+    void setStation(Station *station);
+
+    int getNumber() const;
+
+    void setNumber(int number);
+
+    bool properlyInitialized();
 
 };
 

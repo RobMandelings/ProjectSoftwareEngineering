cd89ade8c9831b39e0168953021abd4f13e59e1f
Jasper De Laet <jdlaet@gmail.com>
2020-06-09 17:47:51 +0200

    framework output test added

--- src/tests/outputTest.cpp
+++ src/tests/outputTest.cpp
@@ -4,6 +4,9 @@
 
 #include <gtest.h>
 #include <fstream>
+#include <time/Timer.h>
+#include <utils/constants.h>
+#include <time/SimulationTime.h>
 #include "metro_utils.h"
 #include "metro_parser.h"
 #include "simulation_utils.h"
@@ -40,6 +43,36 @@ TEST(OutputTest, Positive_graphicalImpression){
     strTest << t.rdbuf();
 
     EXPECT_EQ(strExample.str(), strTest.str());
+}
+
+TEST(OutputTest, simulation_run){
+    srand(time(0));
+
+    MetroNet* metroNet = NULL;
+
+    try {
+        metroNet = metro_parser::parseMetroNetXml("../input/voorbeeldComplex.xml");
+        std::vector<long> returnTimesMillis;
+        returnTimesMillis.push_back(5000);
+        returnTimesMillis.push_back(10000);
+        if (simulation_utils::simulateTrams(*metroNet, returnTimesMillis)) {
+            std::cout << "Simulation returned at " << SimulationTime::get().getFormattedTime() << std::endl;
+            Timer::get().setUpdateTime();
+            usleep(1 / (float) constants::UPDATES_PER_SECOND * 1e6);
+            simulation_utils::simulateTrams(*metroNet, returnTimesMillis);
+
+            // Write tests...
+            
+            
+        } else {
+            std::cout << "Simulation ended at " << SimulationTime::get().getFormattedTime() << std::endl;
+        }
+        metro_utils::printMetroNet(metroNet, "../output/Summary.metro");
+        metro_utils::getGraphicalImpression("../output/Summary.metro", "../output/graphicalLines.metro");
+        delete metroNet;
+    } catch (const metro_parser::MetroNetParseException& e) {
+        std::cerr << e.what();
+    }
 }
 
 int main(int argc, char **argv) {
